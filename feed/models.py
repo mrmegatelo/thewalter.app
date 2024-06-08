d30c9b1b092066bbb90146a5760bcfb693f0edfb
Mikhail Ivanov <ya.ivanov-m2014@yandex.ru>
2024-06-08 16:48:12 +0200

    some settings and .gitignore tweaks

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Feed(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    link = models.URLField()
+    pub_date = models.DateTimeField()
+    guid = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class FeedItem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    link = models.URLField()
+    pub_date = models.DateTimeField()
+    guid = models.CharField(max_length=200, unique=True)
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
