2847bb85207d853640c40f7d82f12f13e60be2d7
Mikhail Ivanov <ya.ivanov-m2014@yandex.ru>
2024-06-12 09:05:00 +0200

    auth system

--- feed/urls.py
+++ feed/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from . import views
@@ -6,4 +7,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('new_feed/', views.NewFeed.as_view(), name='new_feed'),
     path('feed_success/', views.FeedSuccess.as_view(), name='feed_success'),
+
+    path('login/', LoginView.as_view(next_page='index'), name='login'),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,11 +1,16 @@
 from django.views.generic import TemplateView, CreateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from feed.forms import FeedForm
 from feed.models import Feed
 
 
+class ProtectedViewMixin(LoginRequiredMixin):
+    login_url = '/login/'
+
+
 # Create your views here.
-class IndexView(ListView):
+class IndexView(ProtectedViewMixin, ListView):
     model = Feed
     template_name = 'index.html'
 
@@ -15,11 +20,11 @@ class IndexView(ListView):
         return context
 
 
-class FeedSuccess(TemplateView):
+class FeedSuccess(ProtectedViewMixin, TemplateView):
     template_name = 'feed/success.html'
 
 
-class NewFeed(CreateView):
+class NewFeed(ProtectedViewMixin, CreateView):
     form_class = FeedForm
     template_name = 'feed/new.html'
     success_url = '/feed_success/'
