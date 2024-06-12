d3b5f286e80d5ae9cc01923830a975a9c2e951bd
Mikhail Ivanov <ya.ivanov-m2014@yandex.ru>
2024-06-12 08:25:16 +0200

    periodic parsing

--- feed/tasks.py
+++ feed/tasks.py
@@ -16,12 +16,23 @@ def parse_feed(pk):
     try:
         feed_data = feedparser.parse(feed.rss_url)
         for entry in feed_data.entries:
-            FeedItem.objects.create(
-                title=entry.title,
-                description=entry.summary,
-                link=entry.link,
-                pub_date=time.strftime('%Y-%m-%dT%H:%M:%SZ', entry.published_parsed),
-                feed=feed
-            )
+            if not FeedItem.objects.filter(title=entry.title).exists():
+                FeedItem.objects.create(
+                    title=entry.title,
+                    description=entry.summary,
+                    link=entry.link,
+                    pub_date=time.strftime('%Y-%m-%dT%H:%M:%SZ', entry.published_parsed),
+                    feed=feed
+                )
     except Exception as e:
         print(e)
+
+
+@shared_task()
+def update_feeds():
+    """
+    Update all feeds
+    :return:
+    """
+    for feed in Feed.objects.all():
+        parse_feed.delay(feed.pk)
