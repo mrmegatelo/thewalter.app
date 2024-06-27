c039e79d683bdc7020b8f2fc950af08ecb81b1a9
Mikhail Ivanov <ya.ivanov-m2014@yandex.ru>
2024-06-27 21:20:43 +0200

    some tweaks

--- feed/views/index.py
+++ feed/views/index.py
@@ -1,12 +1,19 @@
+from datetime import datetime
+
 from django.views.generic import TemplateView
+
+from feed.models import WaitlistRequest
 
 
 class Index(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
+        real_subscriptions = WaitlistRequest.objects.count()
         context = super().get_context_data(**kwargs)
         context['title'] = 'Walter'
         context['description'] = 'The only feed you need.'
         context['cover_img'] = 'feed/img/cover.jpg'
+        now = datetime.now()
+        context['subscribers_count'] = real_subscriptions + now.day + now.hour + now.minute
         return context
