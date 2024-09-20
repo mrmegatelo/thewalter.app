2983bad3c2ad0fc434dd64b19b7b2d283218c2a4
Mikhail Ivanov <ya.ivanov-m2014@yandex.ru>
2024-09-20 14:58:09 +0200

    fixes some bugs

--- feed/views/feed/subscription.py
+++ feed/views/feed/subscription.py
@@ -1,9 +1,10 @@
 from django.views.generic import DetailView
 
 from feed.models import Feed
+from feed.views.feed.generic_feed_items_list import FeedFiltersMixin
 
 
-class Subscription(DetailView):
+class Subscription(DetailView, FeedFiltersMixin):
     model = Feed
 
     def get_context_data(self, **kwargs):
