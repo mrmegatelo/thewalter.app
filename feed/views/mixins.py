822c2695ef62e2e850af1b21c75009c8ac53511e
Mikhail Ivanov <ya.ivanov-m2014@yandex.ru>
2024-10-14 20:42:38 +0200

    small fix for the feed

--- feed/views/mixins.py
+++ feed/views/mixins.py
@@ -6,22 +6,24 @@ from feed.utils.helpers import filter_by_attachments_type
 
 
 class PageMetaMixin(ContextMixin):
-    title = 'thewalter.app'
-    description = _('The only feed you need.')
-    cover = 'feed/img/cover.jpg'
+    title = "thewalter.app"
+    description = _("The only feed you need.")
+    cover = "feed/img/cover.jpg"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update({
-            'title': self.title,
-            'description': self.description,
-            'cover': self.cover,
-        })
+        context.update(
+            {
+                "title": self.title,
+                "description": self.description,
+                "cover": self.cover,
+            }
+        )
         return context
 
 
 class FeedFiltersMixin(ContextMixin, View):
-    applied_filters = {'not_interesting': True}
+    applied_filters = {"not_interesting": True}
 
     def setup(self, request, *args, **kwargs):
         filters = self.init_filters(request)
@@ -33,27 +35,33 @@ class FeedFiltersMixin(ContextMixin, View):
         return super().setup(request, *args, **kwargs)
 
     def init_filters(self, request):
-        request_params = request.GET if request.method == 'GET' else request.POST
-        return request_params.getlist('filter')
+        request_params = request.GET if request.method == "GET" else request.POST
+        return request_params.getlist("filter")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['applied_filters'] = self.applied_filters
+        context["applied_filters"] = self.applied_filters
         return context
 
 
 class FeedContextMixin(ContextMixin, View):
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        feed_type = self.request.GET.get('feed_type')
-        context['feed_id'] = self.kwargs.get('feed_id')
-        context['feed_type'] = feed_type
-        context['feed_list'] = Feed.objects.all()
+        feed_type = self.request.GET.get("feed_type")
+        context["feed_id"] = self.kwargs.get("feed_id")
+        context["feed_type"] = feed_type
+        context["feed_list"] = Feed.objects.filter(subscribers=self.request.user)
 
-        if context['feed_type'] is not None:
-            filtered_feed = filter_by_attachments_type(FeedItem.objects, feed_type).values_list('id',
-                                                                                                flat=True).distinct()
-            context['feed_list'] = context['feed_list'].filter(id__in=filtered_feed)
+        # TODO: this is a suboptimal way to retrieve the feeds by their feed item types
+        #  Need to find a better way to do it.
+        if context["feed_type"] is not None:
+            filtered_feed = (
+                filter_by_attachments_type(FeedItem.objects, feed_type)
+                .values_list("id", flat=True)
+                .distinct()
+            )
+            context["feed_list"] = (
+                context["feed_list"].filter(feed_items__in=filtered_feed).distinct()
+            )
 
         return context
