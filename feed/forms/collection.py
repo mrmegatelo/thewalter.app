48f8c13fbc62b8b2709310811e7385e29db5d626
Mikhail Ivanov <ya.ivanov-m2014@yandex.ru>
2024-12-14 14:26:10 +0100

    fixes the feed filtering for the collection creation form

--- feed/forms/collection.py
+++ feed/forms/collection.py
@@ -1,10 +1,18 @@
 from django.forms import HiddenInput, TextInput, CheckboxSelectMultiple
 from django.forms.models import ModelForm
 
-from feed.models import Collection
+from feed.models import Collection, Feed
 
 
 class CollectionForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        user = kwargs.get("initial").get("user")
+        super().__init__(*args, **kwargs)
+        if user is not None:
+            self.fields["feeds"].queryset = Feed.objects.filter(
+                subscribers=user
+            ).filter(collection__isnull=True)
+
     class Meta:
         fields = ["title", "feeds", "user"]
         model = Collection
