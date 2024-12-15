2f8a1ce844253cc8a502c01d284e0b64265895c6
Mikhail Ivanov <ya.ivanov-m2014@yandex.ru>
2024-12-15 21:46:27 +0100

    search functionality

--- feed/views/generic/feed_items_list.py
+++ feed/views/generic/feed_items_list.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.utils.translation import gettext_noop as _
 from django.views.generic import ListView
 
@@ -11,35 +12,9 @@ class GenericFeedItemListView(FeedFiltersMixin, PageMetaMixin, ListView):
     title = _("My feed")
 
     def get_queryset(self):
-        not_interesting = self.applied_filters.get("not_interesting", False)
-        paid = self.applied_filters.get("paid", False)
-        viewed = self.applied_filters.get("viewed", False)
-
-        temp_viewed = self.request.session.get(f"viewed_{self.request.path}", [])
-
         base_queryset = super().get_queryset()
-
-        if not_interesting:
-            not_interesting_qs = (
-                self.request.user.servicefeed_set.filter(type=ServiceFeed.Type.DISLIKED)
-                .first()
-                .feed_items.all()
-            )
-            base_queryset = base_queryset.exclude(id__in=not_interesting_qs)
-
-        if paid:
-            paid_qs = self.model.objects.filter(has_paid_content=True)
-            base_queryset = base_queryset.exclude(id__in=paid_qs)
-
-        if viewed:
-            viewed_qs = (
-                self.request.user.servicefeed_set.filter(type=ServiceFeed.Type.VIEWED)
-                .first()
-                .feed_items.all()
-            )
-            base_queryset = base_queryset.exclude(id__in=viewed_qs).exclude(
-                id__in=temp_viewed
-            )
+        base_queryset = self.__get_queryset_with_filters(base_queryset)
+        base_queryset = self.__get_queryset_with_search(base_queryset)
         return base_queryset
 
     def get_context_data(self, **kwargs):
@@ -56,3 +31,41 @@ class GenericFeedItemListView(FeedFiltersMixin, PageMetaMixin, ListView):
     @property
     def applied_filters_str(self):
         return "&".join([f"filter={k}" for k, v in self.applied_filters.items() if v])
+
+    def __get_queryset_with_search(self, queryset):
+        search_term = self.request.GET.get("search")
+        if search_term:
+            return queryset.filter(
+                Q(title__icontains=search_term) | Q(description__icontains=search_term)
+            )
+        return queryset
+
+    def __get_queryset_with_filters(self, queryset):
+        not_interesting = self.applied_filters.get("not_interesting", False)
+        paid = self.applied_filters.get("paid", False)
+        viewed = self.applied_filters.get("viewed", False)
+
+        temp_viewed = self.request.session.get(f"viewed_{self.request.path}", [])
+
+        if not_interesting:
+            not_interesting_qs = (
+                self.request.user.servicefeed_set.filter(type=ServiceFeed.Type.DISLIKED)
+                .first()
+                .feed_items.all()
+            )
+            queryset = queryset.exclude(id__in=not_interesting_qs)
+
+        if paid:
+            paid_qs = self.model.objects.filter(has_paid_content=True)
+            queryset = queryset.exclude(id__in=paid_qs)
+
+        if viewed:
+            viewed_qs = (
+                self.request.user.servicefeed_set.filter(type=ServiceFeed.Type.VIEWED)
+                .first()
+                .feed_items.all()
+            )
+            queryset = queryset.exclude(id__in=viewed_qs).exclude(
+                id__in=temp_viewed
+            )
+        return queryset
