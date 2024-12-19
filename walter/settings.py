0f41519ac86ed38cee8c85bc035029169322f384
Mikhail Ivanov <ya.ivanov-m2014@yandex.ru>
2024-12-19 09:24:51 +0100

    sets up the initial profiling and tracing

--- walter/settings.py
+++ walter/settings.py
@@ -188,4 +188,14 @@ if not DEBUG:
     DEFAULT_FROM_EMAIL = "no-reply@thewalter.app"
 
     if SENTRY_DSN:
-        sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()])
+        sentry_sdk.init(
+            dsn=SENTRY_DSN,
+            integrations=[DjangoIntegration()],
+            # Set traces_sample_rate to 1.0 to capture 100%
+            # of transactions for tracing.
+            traces_sample_rate=1.0,
+            # Set profiles_sample_rate to 1.0 to profile 100%
+            # of sampled transactions.
+            # We recommend adjusting this value in production.
+            profiles_sample_rate=1.0,
+        )
