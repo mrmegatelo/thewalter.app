173bad1835b302e56cbf6453bad73dae0da72cf5
Mikhail Ivanov <ya.ivanov-m2014@yandex.ru>
2025-01-20 21:27:02 +0100

    removes unused middlewares

--- walter/settings.py
+++ walter/settings.py
@@ -65,8 +65,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "feed.middlewares.queue_mark_viewed",
-    "feed.middlewares.get_viewed",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
